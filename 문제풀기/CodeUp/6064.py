a9ada0fb1c5ecc6e7de964722e16db47945ea54c
kleenex1 <curian4881@gmail.com>
2022-07-12 22:02:53 +0900

    CodeUp풀이

---
+++ 문제풀기/CodeUp/6064.py
@@ -0,0 +1,7 @@
+a, b, c = input().split()
+a = int(a)  #변수 a에 저장되어있는 값을 정수로 바꾸어 다시 변수 a에 저장
+b = int(b)
+c = int(c)
+
+d = (a if a<b else b) if ((a if a<b else b)<c) else c
+print(d)
---
+++ 문제풀기/CodeUp/6070.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+if 3 <= n < 6:
+    print("spring")
+elif 6 <= n < 9:
+    print("summer")
+elif 9 <= n < 12:
+    print("fall")
+elif n == 12 or 1 <= n :
+    print("winter")

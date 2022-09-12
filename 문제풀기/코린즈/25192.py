3f8a61d4dd2f3b17f180e6b1b20525aaa6c796d3
kleenex1 <curian48@naver.com>
2022-09-12 17:30:26 +0900

    boj

--- 문제풀기/코린즈/25192.py
+++ 문제풀기/코린즈/25192.py
@@ -1,3 +1,20 @@
 import sys
 
 sys.stdin = open("_인사성밝은곰곰이.txt")
+input = sys.stdin.readline
+N = int(input())
+
+gom = dict()
+answer = 0
+for i in range(N):
+    S = input()
+    if S == "ENTER":
+        answer += sum(gom.values())
+        gom = dict()
+    else:
+        gom[S] = 1
+
+answer += sum(gom.values())
+print(answer)
+
+

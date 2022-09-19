723fd324064ce7ad32d40f87fe0ab5be1985a8b2
kleenex1 <curian48@naver.com>
2022-09-19 20:13:32 +0900

    boj

--- 문제풀기/코린즈/5533.py
+++ 문제풀기/코린즈/5533.py
@@ -7,16 +7,15 @@ N = int(input())
 games = [list(map(int,input().split())) for _ in range(N)]
 
 
-games_set = []
-for j in range(len(games[0])):
-    game_set = []
-    for i in range(N):
-        game_set.append(games[i][j])
-    games_set.append(game_set)
+total_set = [[0]*N for _ in range(3)]
+for i in range(3):
+    for j in range(N):
+        total_set[i][j] = games[j][i]
 
+print(total_set)
 for i in range(N):
-    for j in range(len(games[0])):
-        if games_set[j].count(games[i][j]) > 1:
+    for j in range(3):
+        if total_set[j].count(games[i][j]) > 1:
             games[i][j] = 0
 
 for i in games:

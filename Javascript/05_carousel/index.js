0c373c3277a2e4cc193a313789079d7f82f58273
kleenex1 <curian48@naver.com>
2022-09-20 15:44:03 +0900

    lotto

--- Javascript/05_carousel/index.js
+++ Javascript/05_carousel/index.js
@@ -1,21 +1,56 @@
-const prevBtn = document.querySelector('#prevBtn')
-const currentElement = document.querySelector('.active')
-const items = document.querySelectorAll('.carousel-item')
-const idx = [...items].indexOf(currentElement)
-const nextBtn = document.querySelector('#nextBtn')
+// const prevBtn = document.querySelector('#prevBtn')
+// const currentElement = document.querySelector('.active')
+// const items = document.querySelectorAll('.carousel-item')
+// const idx = [...items].indexOf(currentElement)
+// const nextBtn = document.querySelector('#nextBtn')
 
+// function preBtn() {
+//   console.log(currentElement, items, idx)
+//   currentElement.classList.toggle('active')
+//   items[((idx-1)+items.length)%items.length].classList.toggle('active')
+// }
+
+// function nexBtn() {
+//   console.log(currentElement, items, idx)
+//   currentElement.classList.toggle('active')
+//   items[(idx+1)%items.length].classList.toggle('active')
+// }
+
+// prevBtn.addEventListener('click', preBtn)
+// nextBtn.addEventListener('click', nexBtn)
+
+// setInterval(function() {
+//   const nextBtn = document.querySelector('#nextBtn')
+//   nextBtn.click()
+// }, 10000)
+
+// document.addEventListener('load', function() {
+
+// })
+
+const prevBtn = document.querySelector('#prevBtn')
 prevBtn.addEventListener('click', function() {
+  // 지금 active인 것은 어떻게 알죠?
+  const currentElement = document.querySelector('.active')
+  // 전체 item 중에....... 이 Element의 인덱스?
+  const items = document.querySelectorAll('.carousel-item')
+  const idx = [...items].indexOf(currentElement)
   console.log(currentElement, items, idx)
   currentElement.classList.toggle('active')
   items[(items.length+idx-1)%items.length].classList.toggle('active')
 })
 
+const nextBtn = document.querySelector('#nextBtn')
 nextBtn.addEventListener('click', function() {
+  // 지금 active인 것은 어떻게 알죠?
+  const currentElement = document.querySelector('.active')
+  // 전체 item 중에....... 이 Element의 인덱스?
+  const items = document.querySelectorAll('.carousel-item')
+  const idx = [...items].indexOf(currentElement)
   console.log(currentElement, items, idx)
   currentElement.classList.toggle('active')
   items[(idx+1)%items.length].classList.toggle('active')
 })
-
 setInterval(function() {
     const nextBtn = document.querySelector('#nextBtn')
     nextBtn.click()
--- 문제풀기/코린즈/5533.py
+++ 문제풀기/코린즈/5533.py
@@ -7,12 +7,14 @@ N = int(input())
 games = [list(map(int,input().split())) for _ in range(N)]
 
 
+# 행/열 바꿔주기 
 total_set = [[0]*N for _ in range(3)]
+
 for i in range(3):
     for j in range(N):
         total_set[i][j] = games[j][i]
-
 print(total_set)
+# 원래 그래프에서 값 바꿔주기
 for i in range(N):
     for j in range(3):
         if total_set[j].count(games[i][j]) > 1:
